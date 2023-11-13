2e564ccbc01e7b6cd2e39e16f4401f015a3e7f1e
Kamila_Zaeva <kamila_zaeva@epam.com>
2023-11-13 22:14:17 +0100

    fix tests

--- src/components/MovieForm/MovieForm.test.tsx
+++ src/components/MovieForm/MovieForm.test.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
-import MovieForm, { MovieFormProps } from './MovieForm'; // Import your MovieForm component and MovieFormProps
+import MovieForm, { MovieFormProps } from './MovieForm';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 describe('MovieForm Component', () => {
+    // Mock the react-router-dom module
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    jest.mock('react-router-dom', () => ({
+        ...jest.requireActual('react-router-dom'),
+        useNavigate: () => jest.fn(),
+    }));
+
     const mockMovie = {
-        id: 123456,
+        id: 337167,
         movieName: 'Test Movie',
         releaseYear: 2022,
         movieUrl: 'http://example.com',
@@ -16,15 +24,16 @@ describe('MovieForm Component', () => {
         imageUrl: '',
     };
 
-    const mockProps: MovieFormProps = {
-        movie: mockMovie,
-    };
-
     it('renders with correct movie data', () => {
-        const { getByLabelText } = render(<MovieForm movie={mockMovie} />);
+        const { getByLabelText } = render(
+            <Router>
+                <MovieForm movie={mockMovie} />
+            </Router>,
+        );
+
         expect(getByLabelText('Title')).toHaveValue(mockMovie.movieName);
         expect(getByLabelText('Release date')).toHaveValue(String(mockMovie.releaseYear));
-        expect(getByLabelText('Movie url')).toHaveValue(mockMovie.movieUrl);
+        expect(getByLabelText('Movie url')).toHaveValue(mockMovie.imageUrl);
         expect(getByLabelText('Rating')).toHaveValue(mockMovie.voteAverage);
         expect(getByLabelText('Genre')).toHaveValue(mockMovie.genre);
         expect(getByLabelText('Runtime')).toHaveValue(String(mockMovie.duration));
